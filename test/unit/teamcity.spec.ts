709fac0895be60aa0adc63086317a64a4df75999
sasha <sasha@codemotion.co.uk>
2016-06-08 00:26:48 +0100

    added karma, jasmin with test and tdd gulp tasks

---
+++ test/unit/teamcity.spec.ts
@@ -0,0 +1,12 @@
+import {HttpClient} from 'aurelia-fetch-client';
+import {autoinject} from "aurelia-dependency-injection";
+
+@autoinject
+describe('TeamCity queries', ()=>{
+   beforeEach(()=>{
+
+   });
+   it('blah',()=> {
+      expect(true).toBe(true);
+   });
+});
