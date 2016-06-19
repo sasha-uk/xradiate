d973d4389a4b9286bd34661a7aa904f815228b0d
sasha <sasha@codemotion.co.uk>
2016-06-19 15:26:24 +0100

    added simple configuration

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,29 @@
 //import {computedFrom} from 'aurelia-framework';
 
 export class Config {
-  heading = 'config';
+
+    heading = 'config';
+    teamcityUrl:string = '';
+    branchName:string = '';
+    buildTypes:string = '';
+
+    activate() {
+        if (typeof(Storage) != null) {
+            this.teamcityUrl = (localStorage as any).teamcityUrl;
+            this.branchName = (localStorage as any).branchName;
+            this.buildTypes = (localStorage as any).buildTypes;
+        } else {
+            alert('No local storage available!')
+        }
+    }
+
+    submit() {
+        if (typeof(Storage) != null) {
+            (localStorage as any).teamcityUrl = this.teamcityUrl;
+            (localStorage as any).branchName = this.branchName;
+            (localStorage as any).buildTypes = this.buildTypes;
+        } else {
+            alert('No local storage available!')
+        }
+    };
 }
