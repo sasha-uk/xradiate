f067b968ba936a43236af8b53392a22eb509fdd9
sasha <sasha@codemotion.co.uk>
2016-06-27 21:44:03 +0100

    fullscreen available
    http://localhost:9000/#/ for fullscreen board
    http://localhost:9000/#/admin/ for admin
    http://localhost:9000/#/admin/config for config

--- src/admin-router.ts
+++ src/admin-router.ts
@@ -1,13 +1,14 @@
 import {RouterConfiguration, Router} from 'aurelia-router'
 
-export class App {
+export class AdminRouter {
     router:Router;
 
     configureRouter(config:RouterConfiguration, router:Router) {
         config.title = '';
         config.map([
             {route: ['', 'home'], name: 'home', moduleId: './home', nav: true, title: 'home'},
-            {route: 'admin',  name: 'admin-router', moduleId: 'admin-router', nav: true, title: 'Admin' }
+            {route: ['config', 'config'], name: 'config', moduleId: './config', nav: true, title: 'config'},
+            {route: ['test', 'test'], name: 'test', moduleId: './test', nav: true, title: 'test'},
         ]);
         this.router = router;
     }
